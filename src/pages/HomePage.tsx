18e3aa786d9c31433242051c5a95e53de2048157
armanpwnz <arman.zhumanov13@gmail.com>
2022-08-12 11:10:11 +0200

    Added useDebounce
    Added input for search users

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDebounce } from '../hooks/debounce'
 import { useSearchUsersQuery } from '../store/github/github.api'
 
 export const HomePage = () => {
-  const { isLoading, isError, data } = useSearchUsersQuery('vladilen')
-  console.log(data)
+  const [search, setSearch] = useState('')
+  const debounced = useDebounce(search)
+  const { isLoading, isError, data } = useSearchUsersQuery(debounced)
 
-  return <div>HomePage</div>
+  useEffect(() => {
+    console.log(debounced)
+  }, [debounced])
+
+  return (
+    <div className="flex justify-center pt-5 mx-auto h-screen w-screen">
+      {isError && (
+        <p className="text-center text-red-600">Something went wrong...</p>
+      )}
+      <div className="relative w-[560px]">
+        <input
+          type="text"
+          className="border py-2 px-4 w-full h-[42px] mb-2"
+          placeholder="Search for Github username..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
+          nostrum omnis repellat.
+        </div>
+      </div>
+    </div>
+  )
 }
